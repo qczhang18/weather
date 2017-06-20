096ed896fc119ba7c9168f111f975575d4f4723b
Q. Zhang <aznyqjai@gmail.com>
2017-06-20 00:30:49 -0400

    bootstrap react-bootstrap added and styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 const PLACES = [
   { name: "Palo Alto", zip: "94303" },
@@ -55,22 +58,36 @@ class WeatherDisplay extends Component {
     render() {
       const activePlace = this.state.activePlace;
       return (
-        <div className="App">
-          <WeatherDisplay
-            key={activePlace}
-            zip={PLACES[activePlace].zip}
-            />
-          {PLACES.map((place, index) => (
-            <button
-              key={index}
-              onClick={() => {
-                this.setState({ activePlace: index });
-                console.log('Clicked index '+index);
-              }}
-              >
-              {place.name}
-            </button>
-          ))}
+        <div>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                React Simple Weather App
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Navbar>
+          <Grid>
+            <Row>
+              <Col md={4} sm={4}>
+                <h3>Select a city</h3>
+                <Nav
+                  bsStyle="pills"
+                  stacked
+                  activeKey={activePlace}
+                  onSelect={index => {
+                    this.setState({ activePlace: index });
+                  }}
+                >
+                  {PLACES.map((place, index) => (
+                    <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                  ))}
+                </Nav>
+              </Col>
+              <Col md={8} sm={8}>
+                <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+              </Col>
+            </Row>
+          </Grid>
         </div>
       );
     }
